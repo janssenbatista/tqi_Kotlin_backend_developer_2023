ecbb5405dd317b3e8c367c018220a2bb045c2f48
Janssen Batista <batistajanssen.dev@gmail.com>
2023-07-28 18:30:31 -0300

    add api documentation to product controller

--- src/main/kotlin/dev/janssenbatista/tqi_Kotlin_backedn_developer_2023/controllers/ProductController.kt
+++ src/main/kotlin/dev/janssenbatista/tqi_Kotlin_backedn_developer_2023/controllers/ProductController.kt
@@ -3,6 +3,10 @@ package dev.janssenbatista.tqi_Kotlin_backedn_developer_2023.controllers
 import dev.janssenbatista.tqi_Kotlin_backedn_developer_2023.dtos.ProductRequestDto
 import dev.janssenbatista.tqi_Kotlin_backedn_developer_2023.models.Product
 import dev.janssenbatista.tqi_Kotlin_backedn_developer_2023.services.ProductService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
@@ -15,14 +19,29 @@ import java.util.*
 
 @RestController
 @RequestMapping("/products")
+@Tag(name = "Product")
+@SecurityRequirement(name = "bearerAuth")
 class ProductController(private val productService: ProductService) {
 
+    @Operation(
+        summary = "create a product",
+        responses = [
+            ApiResponse(description = "product already exists", responseCode = "409"),
+            ApiResponse(description = "product created", responseCode = "201")
+        ]
+    )
     @PostMapping
     fun createProduct(@RequestBody @Valid dto: ProductRequestDto): ResponseEntity<Product> {
         val createdProduct = productService.save(dto)
         return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct)
     }
 
+    @Operation(
+        summary = "get all products",
+        responses = [
+            ApiResponse(description = "list of products", responseCode = "200")
+        ]
+    )
     @GetMapping
     fun findAllProducts(@PageableDefault(
         page = 0,
@@ -32,12 +51,27 @@ class ProductController(private val productService: ProductService) {
         return ResponseEntity.ok(productsPage)
     }
 
+    @Operation(
+        summary = "find product by id",
+        responses = [
+            ApiResponse(description = "product found", responseCode = "200"),
+            ApiResponse(description = "product not found", responseCode = "404")
+        ]
+    )
     @GetMapping("/{productId}")
     fun findProductById(@PathVariable productId: UUID): ResponseEntity<Product> {
         val foundProduct = productService.findById(productId)
         return ResponseEntity.ok(foundProduct)
     }
 
+    @Operation(
+        summary = "update a product by id",
+        responses = [
+            ApiResponse(description = "product updated", responseCode = "200"),
+            ApiResponse(description = "product not found", responseCode = "404"),
+            ApiResponse(description = "product already exists", responseCode = "409")
+        ]
+    )
     @PutMapping("/{productId}")
     fun updateProduct(@PathVariable productId: UUID,
                       @RequestBody @Valid dto: ProductRequestDto): ResponseEntity<Product> {
@@ -45,6 +79,13 @@ class ProductController(private val productService: ProductService) {
         return ResponseEntity.ok(updatedProduct)
     }
 
+    @Operation(
+        summary = "delete a product by id",
+        responses = [
+            ApiResponse(description = "product deleted", responseCode = "204"),
+            ApiResponse(description = "product not found", responseCode = "404")
+        ]
+    )
     @DeleteMapping("/{productId}")
     fun deleteProduct(@PathVariable productId: UUID): ResponseEntity<Product> {
         productService.delete(productId)
